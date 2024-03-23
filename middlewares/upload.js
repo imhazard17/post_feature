bea098bbe0589b9fe4622756d4b6545022a4daa5
Rajarshi Barman <rajubarman4635@gmail.com>
2024-03-23 09:52:51 +0530

    renamed:    utils/multer.js -> middlewares/upload.js also now file will be to different subdriectories of 'uploads' folder based on the url of the request

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -3,7 +3,13 @@ const crypto = require('crypto')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './uploads/');
+        if(req.url === '/user/auth/signup' || req.url === '/user/change-details') {
+            cb(null, './uploads/profilePics/');
+        }
+        if(req.url === '/post/create' || req.url.startsWith('/post/update')) {
+            cb(null, './uploads/postPics/');
+        }
+        
     },
     filename: (req, file, cb) => {
         cb(null, crypto.randomBytes(15).toString('hex') + '.jpg')
--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const prisma = require('../utils/db')
 const authentication = require('../middlewares/authentication')
-const upload = require('../utils/multer')
+const upload = require('../middlewares/upload')
 const { postInputValidation } = require('../middlewares/input_validation')
 const fs = require('node:fs/promises')
 const errForward = require('../utils/errorForward')
@@ -61,98 +61,94 @@ router.get('/get-all/:username', errForward(
     }))
 
 // POST /post/create
-router.post('/create', [postInputValidation, authentication, upload.array("file", 7)], errForward(
-    async (req, res) => {
-        const userId = req.locals.userId;
+router.post('/create', [postInputValidation, authentication, upload.array("file", 7)], errForward(async (req, res) => {
+    const userId = req.locals.userId;
 
-        if (!req.files) {
-            res.status(404).json({
-                err: 'No images uploaded',
-            })
-            return;
-        }
-
-        const uploadedFilePaths = req.files.map((file) => file.path)
-
-        const createdPost = await prisma.post.create({
-            data: {
-                userId: userId,
-                description: req.body.description,
-                imgUrls: uploadedFilePaths,
-            },
-            select: {
-                id: true
-            }
+    if (!req.files) {
+        res.status(404).json({
+            err: 'No images uploaded',
         })
-
-        if (!createdPost) {
-            res.status(404).json({
-                err: `No username ${username} found`,
-            })
-            return;
-        }
-
-        res.status(200).json({
-            msg: `Successfully created post with post id: ${createdPost.id}`
-        })
+        return;
     }
-))
+
+    const uploadedFilePaths = req.files.map((file) => file.path)
+
+    const createdPost = await prisma.post.create({
+        data: {
+            userId: userId,
+            description: req.body.description,
+            imgUrls: uploadedFilePaths,
+        },
+        select: {
+            id: true
+        }
+    })
+
+    if (!createdPost) {
+        res.status(404).json({
+            err: `No username ${username} found`,
+        })
+        return;
+    }
+
+    res.status(200).json({
+        msg: `Successfully created post with post id: ${createdPost.id}`
+    })
+}))
 
 // PUT /post/update/:id
-router.put('/update/:id', [postInputValidation, authentication, upload.array('file', 7)], errForward(
-    async (req, res) => {
-        const postId = parseInt(req.params.id)
+router.put('/update/:id', [postInputValidation, authentication, upload.array('file', 7)], errForward(async (req, res) => {
+    const postId = parseInt(req.params.id)
 
-        if (!req.files) {
-            res.status(404).json({
-                err: 'No images uploaded',
-            })
-            return;
-        }
-
-        const uploadedFilePaths = req.files.map((file) => file.path)
-
-        const post = await prisma.post.findUnique({
-            where: {
-                id: postId,
-                userId: req.locals.userId,
-            },
-            select: {
-                imgUrls: true
-            }
+    if (!req.files) {
+        res.status(404).json({
+            err: 'No images uploaded',
         })
-
-        const updatedPost = await prisma.post.update({
-            where: {
-                id: postId,
-            },
-            data: {
-                description: req.body.description,
-                imgUrls: uploadedFilePaths,
-            },
-            select: {
-                id: true,
-            }
-        })
-
-        const prevImgPaths = post.imgUrls
-
-        for (let i = 0; i < prevImgPaths.length; i++) {
-            await fs.unlink(prevImgPaths[i])
-        }
-
-        if (!updatedPost) {
-            res.status(404).json({
-                err: 'Could not update post',
-            })
-            return;
-        }
-
-        res.status(200).json({
-            msg: `Successfully updated post with post id: ${updatedPost.id}`
-        })
+        return;
     }
-))
+
+    const uploadedFilePaths = req.files.map((file) => file.path)
+
+    const post = await prisma.post.findUnique({
+        where: {
+            id: postId,
+            userId: req.locals.userId,
+        },
+        select: {
+            imgUrls: true
+        }
+    })
+
+    const updatedPost = await prisma.post.update({
+        where: {
+            id: postId,
+        },
+        data: {
+            description: req.body.description,
+            imgUrls: uploadedFilePaths,
+        },
+        select: {
+            id: true,
+        }
+    })
+
+    const prevImgPaths = post.imgUrls
+
+    for (let i = 0; i < prevImgPaths.length; i++) {
+        await fs.unlink(prevImgPaths[i])
+    }
+
+    if (!updatedPost) {
+        res.status(404).json({
+            err: 'Could not update post',
+        })
+        return;
+    }
+
+    res.status(200).json({
+        msg: `Successfully updated post with post id: ${updatedPost.id}`
+    })
+}))
 
 // DELETE /post/delete/:id
 router.delete('/:id', [authentication], errForward(
